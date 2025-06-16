79fbf3601aa62565efe54e0de10f59a9314f1ebf
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-16 23:00:11 +0000

    Code pair #p1

--- code pair #p1/refactored_code.py
+++ code pair #p1/refactored_code.py
@@ -3,13 +3,11 @@
 
 
 
-# original code
- def spider_opened(self, spider: Spider) -> None:
+# refactored code
+def spider_opened(self, _spider: Spider) -> None:
         self.time_prev: datetime = datetime.now(tz=timezone.utc)
         self.delta_prev: Dict[str, Union[int, float]] = {}
         self.stats_prev: Dict[str, Union[int, float]] = {}
 
         self.task = task.LoopingCall(self.log)
         self.task.start(self.interval)
-
-

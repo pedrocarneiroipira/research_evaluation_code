8d7808ba19641ad12b74ad9d7e3b01faf0344dcb
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-20 17:21:20 +0000

    Par 2

--- code pair #p1/code_b.py
+++ code pair #p1/code_b.py
@@ -3,7 +3,6 @@
 
 
 
-# refactored code
 def spider_opened(self, _spider: Spider) -> None:
         self.time_prev: datetime = datetime.now(tz=timezone.utc)
         self.delta_prev: Dict[str, Union[int, float]] = {}

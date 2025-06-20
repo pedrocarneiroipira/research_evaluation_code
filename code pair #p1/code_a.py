1fdc20db3bf7db02826b75f70d3cddb9d3deb8d5
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-20 17:18:48 +0000

    Par 1

--- code pair #p1/code_a.py
+++ code pair #p1/code_a.py
@@ -1,16 +1,14 @@
 # Code pair #p1
-# LLM Model: gpt-3.5-turbo
-
-# S1772: Unused function parameters should be removed
-# applied refactoring technique: variable renaming
+# Code A
 
 
 
-# refactored code
-def spider_opened(self, _spider: Spider) -> None:
+def spider_opened(self, spider: Spider) -> None:
         self.time_prev: datetime = datetime.now(tz=timezone.utc)
         self.delta_prev: Dict[str, Union[int, float]] = {}
         self.stats_prev: Dict[str, Union[int, float]] = {}
 
         self.task = task.LoopingCall(self.log)
         self.task.start(self.interval)
+
+
--- code pair #p1/code_b.py
+++ code pair #p1/code_b.py
@@ -1,15 +1,13 @@
 # Code pair #p1
-# LLM Model: gpt-3.5-turbo
+# Code B
 
 
 
-# original code
- def spider_opened(self, spider: Spider) -> None:
+# refactored code
+def spider_opened(self, _spider: Spider) -> None:
         self.time_prev: datetime = datetime.now(tz=timezone.utc)
         self.delta_prev: Dict[str, Union[int, float]] = {}
         self.stats_prev: Dict[str, Union[int, float]] = {}
 
         self.task = task.LoopingCall(self.log)
         self.task.start(self.interval)
-
-

d5841d52bb1d798a1fe31f2cd448968c56353b65
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-25 16:11:53 +0000

    Update pair

--- code pair #p08/code_b.py
+++ code pair #p08/code_b.py
@@ -2,67 +2,27 @@
 # Code B
 
 
-
-def display_for_field(value, field, empty_value_display):
-    from django.contrib.admin.templatetags.admin_list import _boolean_icon
-
-    if getattr(field, "flatchoices", None):
-        return _handle_flatchoices(value, field, empty_value_display)
-    elif isinstance(field, models.BooleanField):
-        return _handle_boolean_field(value, _boolean_icon)
-    elif value in field.empty_values:
-        return empty_value_display
-    elif isinstance(field, models.DateTimeField):
-        return _handle_datetime_field(value)
-    elif isinstance(field, (models.DateField, models.TimeField)):
-        return _handle_date_or_time_field(value)
-    elif isinstance(field, models.DecimalField):
-        return _handle_decimal_field(value, field)
-    elif isinstance(field, (models.IntegerField, models.FloatField)):
-        return _handle_numeric_field(value)
-    elif isinstance(field, models.FileField) and value:
-        return _handle_file_field(value)
-    elif isinstance(field, models.JSONField) and value:
-        return _handle_json_field(value, field, empty_value_display)
+def next_flow(self, flow: http.HTTPFlow) -> http.HTTPFlow | None:
+    """
+    Returns the next flow object, or None if no matching flow was
+    found.
+    """
+    flow_hash = self._hash(flow)
+    if flow_hash in self.flowmap:
+        if ctx.options.server_replay_reuse or ctx.options.server_replay_nopop:
+            return next(
+                (flow for flow in self.flowmap[flow_hash] if flow.response), None
+            )
+        else:
+            ret = self.flowmap[flow_hash].pop(0)
+            while not ret.response:
+                if self.flowmap[flow_hash]:
+                    ret = self.flowmap[flow_hash].pop(0)
+                else:
+                    del self.flowmap[flow_hash]
+                    return None
+            if not self.flowmap[flow_hash]:
+                del self.flowmap[flow_hash]
+            return ret
     else:
-        return display_for_value(value, empty_value_display)
-
-
-def _handle_flatchoices(value, field, empty_value_display):
-    try:
-        return dict(field.flatchoices).get(value, empty_value_display)
-    except TypeError:
-        flatchoices = make_hashable(field.flatchoices)
-        value = make_hashable(value)
-        return dict(flatchoices).get(value, empty_value_display)
-
-
-def _handle_boolean_field(value, _boolean_icon):
-    return _boolean_icon(value)
-
-
-def _handle_datetime_field(value):
-    return formats.localize(timezone.template_localtime(value))
-
-
-def _handle_date_or_time_field(value):
-    return formats.localize(value)
-
-
-def _handle_decimal_field(value, field):
-    return formats.number_format(value, field.decimal_places)
-
-
-def _handle_numeric_field(value):
-    return formats.number_format(value)
-
-
-def _handle_file_field(value):
-    return format_html('<a href="{}">{}</a>', value.url, value)
-
-
-def _handle_json_field(value, field, empty_value_display):
-    try:
-        return json.dumps(value, ensure_ascii=False, cls=field.encoder)
-    except TypeError:
-        return display_for_value(value, empty_value_display)
+        return None

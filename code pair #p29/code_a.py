2b84a5b024d1da92eaa51c4124037b076c60214c
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-25 16:15:48 +0000

    Update pair

--- code pair #p29/code_a.py
+++ code pair #p29/code_a.py
@@ -2,14 +2,13 @@
 # Code A
 
 
-
 def decode(self, encoded):
-        algorithm, empty, algostr, work_factor, data = encoded.split("$", 4)
-        assert algorithm == self.algorithm
-        return {
-            "algorithm": algorithm,
-            "algostr": algostr,
-            "checksum": data[22:],
-            "salt": data[:22],
-            "work_factor": int(work_factor),
-        }
+    algorithm, empty, algostr, work_factor, data = encoded.split("$", 4)
+    assert algorithm == self.algorithm
+    return {
+        "algorithm": algorithm,
+        "algostr": algostr,
+        "checksum": data[22:],
+        "salt": data[:22],
+        "work_factor": int(work_factor),
+    }

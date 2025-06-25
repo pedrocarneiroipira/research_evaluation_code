c485ff7491a0e2487c282881bda413b105069494
Pedro Carneiro de Souza <169102383+pedrocarneiroipira@users.noreply.github.com>
2025-06-25 16:17:30 +0000

    Update pair

--- code pair #p34/code_a.py
+++ code pair #p34/code_a.py
@@ -2,15 +2,12 @@
 # Code A
 
 
-
 def divide() -> None:
-        number = 1
-        divisor = 0
-        foos = ["foo"] * 100
-        log.debug("in divide")
-        try:
-            number / divisor
-        except:
-            log.exception("An error of some kind occurred!")
-
-    
+    number = 1
+    divisor = 0
+    foos = ["foo"] * 100
+    log.debug("in divide")
+    try:
+        number / divisor
+    except:
+        log.exception("An error of some kind occurred!")
